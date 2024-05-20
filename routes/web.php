20de5a1636a804132133e6d957a7b5b24c16ae2e
Artem Pylhun <145591080+Artem-Pylhun@users.noreply.github.com>
2024-05-20 18:48:22 +0300

    feat: Lab 3 finished

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Blog\PostController;
+use App\Http\Controllers\RestTestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +17,7 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
+});
+Route::resource('rest', RestTestController::class)->names('restTest');
